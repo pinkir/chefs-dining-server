d4950602a9a47e866f8028cd1d42619a38bdadf9
pinkir <pinki.roy@outlook.com>
2023-05-03 21:17:00 +0600

    filtered chefsrecipes

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ app.get('/recipes', (req, res) =>{
   res.send(recipes);
 })
 
+app.get('/recipes/:id', (req, res) =>{
+  const id = req.params.id;
+  // console.log(id);
+  const selectedRecipes = recipes.find(r => r.id == id);
+  res.send(selectedRecipes);
+})
+
+app.get('/chefs/:id', (req, res)=>{
+  const id =req.params.id;
+  // console.log(id);
+  const chefsRecipes = recipes.filter(r => r.chef_id == id);
+  res.send(chefsRecipes);
+})
 
 app.listen(port, () => {
   console.log(`chefs dining running in server ${port}`)
