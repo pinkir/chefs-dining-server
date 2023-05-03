e664aad4611f6f78b23650a7bdaf6f7a48ff05eb
pinkir <pinki.roy@outlook.com>
2023-05-03 13:58:59 +0600

    chefs and recipe json added

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
 
 app.get('/', (req, res) => {
   res.send('chefs dining is running')
 })
+
+app.get('/chefs', (req, res) =>{
+  res.send(chefs);
+})
+
+app.get('/recipes', (req, res) =>{
+  res.send(recipes);
+})
+
 
 app.listen(port, () => {
   console.log(`chefs dining running in server ${port}`)
